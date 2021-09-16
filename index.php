5bfdb624689e9cbff53ff63360c5683023b74332
Bhautik Jani <83449830+BhautikJani12@users.noreply.github.com>
2021-09-16 15:00:44 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/navbar.css">
+    <link rel="stylesheet" type="text/css" href="css/image.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@600&display=swap" rel="stylesheet">
+
+    <title>Bank of America</title>
+  </head>
+
+  <body style="background-color: black;">
+  <header>
+  <?php
+  include 'navbar.php';
+  ?>
+  </header>
+
+  <section >
+    <h3>About us</h3>
+    <p class="p1">Bank of America, operating in India since 1964, has a well-established track record in the local financial markets. Incorporated in the US, the bank offers a comprehensive suite of financial products including lending & more.</p>
+
+    <h4>Contact us</h4>
+    <p class="p1">bankofamerica.com/Services</p>
+  </section>
+ 
+   <footer class="text-center mt-5 py-2">
+                <p>&copy 2021.<b>BANK OF AMERICA</b> <br> BY Bhautik Jani</p>
+   </footer>   
+
+  </body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,22 @@
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@600&display=swap" rel="stylesheet">
+
+
+<nav class="navbar navbar-expand-md navbar-light ">
+      <a class="navbar-brand" href="index.php" style="color : red;"><b>BANK OF AMERICA</b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php" style="color : red;"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php" style="color :red;"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php" style="color : red;"><b>Transaction History</b></a>
+              </li>
+          </div>
+       </nav>
